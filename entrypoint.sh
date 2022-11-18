f28e71db5ff3cfb8b763ba98e347e8a7fb923297
Rmx1921 <koreanplay14340@gmail.com>
2022-11-18 13:46:59 +0530

    copy img to workdir-fix-1

--- entrypoint.sh
+++ entrypoint.sh
@@ -219,12 +219,12 @@ set_output elapsed_time "$(echo "$(date +%s)"-"$start_time" | bc)"
 msg "Packaging the kernel..."
 zip_filename="${name}-${tag}.zip"
 if [[ -e "$workdir"/"$zipper_path" ]]; then
+    cp out/arch/"$arch"/boot/"$image" "$workdir"/"image"
     cp out/arch/"$arch"/boot/"$image" "$workdir"/"$zipper_path"/"$image"
     cd "$workdir"/"$zipper_path" || exit 127
     rm -rf .git
     zip -r9 "$zip_filename" . -x .gitignore README.md || exit 127
     set_output outfile "$workdir"/"$zipper_path"/"$zip_filename"
-    cp out/arch/"$arch"/boot/"$image" "$workdir"/"image"
     cd "$workdir" || exit 127
     exit 0
 else
